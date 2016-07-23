5a8a07cfa03292d86d06cdcc0efd34bd8a37f13a
burcak156 <burcak156@gmail.com>
2016-07-23 17:45:15 +0300

    Measurement classes
    Measurement Service-Repo-Exc. eklendi. User Repo güncellendi.

---
+++ src/main/java/com/udogan/baklavaborek/exception/MeasurementAlreadyAddedException.java
@@ -0,0 +1,9 @@
+package com.udogan.baklavaborek.exception;
+
+public class MeasurementAlreadyAddedException extends Exception {
+
+	public MeasurementAlreadyAddedException(String msg) {
+		super(msg);
+	}
+	
+}
--- src/main/java/com/udogan/baklavaborek/repository/UserRepository.java
+++ src/main/java/com/udogan/baklavaborek/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.udogan.baklavaborek.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,8 @@ import com.udogan.baklavaborek.entity.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
     User findByEmail(String email);
+    List<User> findAll();
+    User findOne(Long id);
+    <S extends User>S saveAndFlush(S user);
+    void delete(User user);
 }
